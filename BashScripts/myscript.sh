bd17e62aba55785630362b1b28f6336ab41abe83
Ifelowo Emmanuel <109631775+Zmmayin@users.noreply.github.com>
2023-03-04 18:40:34 +0100

    Add files via upload

---
+++ BashScripts/myscript.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Asample Bash script, by Emmanuel
+
+echo Hello World!
---
+++ Variables/simplevariables.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# A simple variable example
+
+myvariable=Hello
+
+anothervar=Emmanuel
+
+echo $myvariable $anothervar
+echo
+
+sampledir=/etc
+
+ls $sampledir
